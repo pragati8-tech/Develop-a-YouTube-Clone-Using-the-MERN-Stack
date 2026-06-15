058126c4e7d0de4c7df8500710303000218226e0
pragatiagrawal69 <your.email@example.com>
2026-06-15 21:40:52 +0530

    initialize VideoPlayer page

---
+++ src/pages/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,9 @@
+const VideoPlayer = () => {
+  return (
+    <div style={{ padding: "20px", color: "#fff" }}>
+      <h2>VideoPlayer Page — Coming Soon</h2>
+    </div>
+  );
+};
+
+export default VideoPlayer;
