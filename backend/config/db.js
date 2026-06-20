52a54f1b2564dd90d8c01586569ae6c5cec2bc8f
pragatiagrawal69 <your.email@example.com>
2026-06-20 21:58:13 +0530

    add MongoDB connection config

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`MongoDB Connection Error: ${error.message}`);
+    process.exit(1); // server band kar do agar DB connect na ho
+  }
+};
+
+export default connectDB;
